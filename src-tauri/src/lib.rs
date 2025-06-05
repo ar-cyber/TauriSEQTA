b70dc03e7222b7bfec1779f1b6b7d1b4b5b72c37
Not Not Not Andrew <loafing@sockycat.net>
2025-06-06 09:08:10 +0930

    remove shell plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,7 +25,6 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_deep_link::init());
